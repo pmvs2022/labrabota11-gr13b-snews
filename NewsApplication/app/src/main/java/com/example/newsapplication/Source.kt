e7320e77d87f149ee9e4a1bc792c9ac5d9158647
avtorgget16@gmail.com <avtorgget16@gmail.com>
2022-12-05 02:18:58 +0300

    setting up retrofit with api

---
+++ NewsApplication/app/src/main/java/com/example/newsapplication/Source.kt
@@ -0,0 +1,6 @@
+package com.example.newsapplication
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ NewsApplication/app/src/main/java/com/example/newsapplication/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapplication.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "f762602fdf4f4106ab2e303d995218a2"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
